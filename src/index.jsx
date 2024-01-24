ee730d45c74d78b7fe906827f1d9ceedbbc58671
christiecamp <christiecamphoto@gmail.com>
2024-01-23 23:52:18 -0500

    did minor adjustments to directory and added index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+//augtogenerated source file for REACT
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
