344102ee4270a104e799f6c5234de274785731a4
christiecamp <christiecamphoto@gmail.com>
2024-01-24 23:46:40 -0500

    changing structure of directory

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Gremlin Land</h1>
+    </div>
+  );
+}
---
+++ src/components/info.js
@@ -0,0 +1,7 @@
+//information for 
+
+//about me
+
+//projects
+
+//skills
