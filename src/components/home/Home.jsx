e1496551619c8dc8b7b6dd727f29651f7b0b98dc
Christine Campbell <christiecamphoto@gmail.com>
2024-02-23 11:02:19 -0500

    added additional branding elements

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Style from './Home.module.scss';
-import gremlin from '../../assets/gremlin.png';
+import gremlins from '../../assets/gremlins.png';
 import {Box} from '@material-ui/core';
 
 
@@ -8,10 +8,15 @@ import {Box} from '@material-ui/core';
 
 //the minimum height of an element is 100vh minus the height of the header and footer. This ensures that the footer is always at the bottom of the page, even if the content is too short to fill the entire viewport.
 
+//using gradients for text
+// linear-gradient(to right, rgb(0, 0, 0), rgb(41, 40, 40));
+
 
 export default function Home() {
     return (
         <Box component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'} justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
+            <Box component={'img'} src={gremlins} className={Style.gremlins} alt={'gremlins'} style={{background: gremlins.gradient}}/>
+            
         </Box>
    )
 }
