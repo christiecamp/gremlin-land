c0779196222ae36091fdb33eea59d7ad2391105c
Christine Campbell <christiecamphoto@gmail.com>
2024-03-07 11:03:29 -0500

    updating sections

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
+//JavaScript (ESM) - use .eslintrc.cjs when running ESLint in JavaScript packages that specify 'type':'module' in their package.json.
+
+//ESLint automatically looks for the file in the directory of the file to be linted, and in successive parent directories all the way up to the root directory of the filesystem (/), the home directory of the current user (~/), or when root: true is specified. 
+
+//ESLint will stop looking in a directory when it finds an .eslintrc.* file containing root: true.
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+//defineConfig helper - function that takes an object and returns a configuration object for Vite.
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//export config object
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(
+    // {
+    //   // autoImport: true,
+    //   // babel: {
+    //   //   plugins: [
+    //   //     ['@babel/plugin-proposal-decorators', { legacy: true }],
+    //   //     ['@babel/plugin-proposal-class-properties', { loose: true }]
+    //   //   ]
+    //   // }
+  )],
   server: {
     port: 3000,
     open: true
